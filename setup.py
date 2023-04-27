fc2df2a603f982f5225122a1888687df8256e996
Mac <kaaso@space.dtu.dk>
2023-04-27 12:04:41 +0200

    add from old

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+  name='ais_python',
+  url='https://github.com/aalling93/ais_python',
+  author='Kristian Aalling Sørensen',
+  author_email='kaaso@space.dtu.dk',
+  packages=['ais_python',],
+  install_requires=['numpy','pandas','cartopy', 'matplotlib'],
+  version='0.1',
+  license='MIT',
+  description='',
+  long_description='see Readme'
+)
+
