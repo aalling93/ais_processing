c4ac1cf65aa8300f6b109d4fbcc23b7fd56729cb
Mac <kaaso@space.dtu.dk>
2023-04-27 12:55:27 +0200

    update pyproject

--- setup.py
+++ setup.py
@@ -5,11 +5,7 @@ setup(
   url='https://github.com/aalling93/ais_processing',
   author='Kristian Aalling Sørensen',
   author_email='kaaso@space.dtu.dk',
-  packages=['ais_processing',],
   install_requires=['numpy'],
   version='0.0.1.1',
-  license='MIT',
-  description='',
-  long_description='see Readme'
 )
 
